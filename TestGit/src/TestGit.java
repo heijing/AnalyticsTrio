e4e8c634f8b58bb4aa2b4666f4557e36a3ca9e25
eleven <eleven@pitt.edu>
2014-08-30 22:07:47 -0400

    Test 2

--- TestGit/src/TestGit.java
+++ TestGit/src/TestGit.java
@@ -3,7 +3,8 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Output");
+		System.out.println("Output"); 
+		
 	}
 
 }
